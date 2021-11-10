302ec7461eaddfb1cc0ac468335b542c496785c6
Balparang <shineecard5@gmail.com>
2021-11-10 21:54:17 +0900

    main.ui - task.ui 연동, task.ui 뼈대

---
+++ Team_GUI/Main/Task/task_email.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtWidgets import QDialog, QApplication
+
+class emailWindow(QDialog):
+    def __init__(self, parent): # 부모 윈도우(메모장)가 있기 때문에 parent 적어주기
+        super(emailWindow, self).__init__(parent)
+        uic.loadUi("Task\\task_email.ui", self)
+        self.setGeometry(500, 500, 600, 400)  # x, y, w, h : 창 크기 조절
+        self.setWindowTitle("e-mail") # 윈도우 타이틀 설정        
+        self.show()
+
+        self.tableWidget.setColumnWidth(0,100)
+        self.tableWidget.setColumnWidth(1,300)
+        
+        self.loadEmail()
+    
+    # 이메일 스크래퍼로부터 정보 받아오는 함수
+    def loadEmail(self):
+        email=[{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"},{"from":"나", "Title":"테스트"}]
+        row = 0
+        self.tableWidget.setRowCount(len(email))
+        for mail in email:
+            self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(mail["from"]))
+            self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(mail["Title"]))
+
+            row += 1
--- Team_GUI/Main/main.py
+++ Team_GUI/Main/main.py
@@ -3,6 +3,11 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
+# import task
+from Task import task_email
+from Task import task_musicplayer
+from Task import task_news
+
 
 # ui 로드
 form_class = uic.loadUiType("main.ui")[0]
@@ -13,6 +18,9 @@ class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setGeometry(500, 500, 600, 400)  # x, y, w, h : 창 크기 조절
+        self.setWindowTitle("Main") # 윈도우 타이틀 설정
+        # self.setFixedSize(600, 400)
         
         ### 기능연결 ###
         # icon 이미지 로드
@@ -21,23 +29,28 @@ class WindowClass(QMainWindow, form_class):
         self.label_icon3.setPixmap(self.loadImageFromFile("image_source\\icon_news.png", 100))
 
         # 버튼 기능연결, lambda: "TypeError: argument 1 has unexpected type 'NoneType'" 방지하기 위해 사용 
-        self.btn_run_email.clicked.connect(lambda: self.runTaskFunction(" # source directory to link ", "email"))
-        self.btn_run_musicplayer.clicked.connect(lambda: self.runTaskFunction(" # source directory to link ", "musicplayer"))
-        self.btn_run_news.clicked.connect(lambda: self.runTaskFunction(" # source directory to link ", "news"))
-        
+        self.btn_run_email.clicked.connect(self.openEmailWindow)        
+        self.btn_run_musicplayer.clicked.connect(self.openMusicPlayerWindow)
+        self.btn_run_news.clicked.connect(self.openNewsWindow)
         
     ### 기능함수 ### 
     # 이미지 로드
-    def loadImageFromFile(self, source_url, width_size): #  source_url의 이미지로 qPixmap 객체생성 후, 해당 객체 리턴
+    def loadImageFromFile(self, source_url, width_size): # source_url의 이미지로 qPixmap 객체생성 후, 해당 객체 리턴
         self.qPixmapFileVar = QPixmap()  # qPixmap 객체 생성
         self.qPixmapFileVar.load(source_url)  # 이미지 로드
         self.qPixmapFileVar = self.qPixmapFileVar.scaledToWidth(width_size)  # 크기 조절
         return self.qPixmapFileVar
     
-    # 버튼 기능 실행
-    def runTaskFunction(self, _source_url, _task): # 
-        print(f"running {_task}...")                
-        _source_url = "test"
+    # dialWindow
+    def openEmailWindow(self):
+        task_email.emailWindow(self)
+        
+    def openMusicPlayerWindow(self):
+        task_musicplayer.musicPlayerWindow(self)
+        
+    def openNewsWindow(self):
+        task_news.newsWindow(self)
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
