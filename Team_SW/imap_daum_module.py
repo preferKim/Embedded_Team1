60272b7fd4db7e87fbd39fee592f6ff8e934c4f0
d3j2d3j2 <013j2@naver.com>
2021-11-12 15:36:57 +0900

    이메일 날짜 형식 수정

--- Team_SW/imap_daum_module.py
+++ Team_SW/imap_daum_module.py
@@ -1,6 +1,7 @@
 import imaplib
 import email
 from email.header import decode_header, make_header
+
 #This code is only for daum.net
 
 def imap_daum(id, pw, num_mails):
@@ -10,43 +11,18 @@ def imap_daum(id, pw, num_mails):
     recent_no = data[0]
 
     mailList = []
-    
+
     for i in range(num_mails):
         mail_no = str(int(recent_no.decode('utf-8'))- i).encode('utf8')
         rv, fetched = server.fetch(mail_no, '(RFC822)')
         message = email.message_from_bytes(fetched[0][1])
 
         date = make_header(decode_header(message.get('Date')))
+        tokenList = str(date).split(' ')
+
+        date = tokenList[2] +" "+tokenList[1] +", "+ tokenList[4][:5]
         fr = make_header(decode_header(message.get('From')))
         subject = make_header(decode_header(message.get('subject')))
 
-        mailList.append([date,fr,subject])
+        mailList.append([date,str(fr),str(subject)])
     return mailList
-
-'''
-mailList = imap_daum('', '', 20)
-
-for i in range(len(mailList)):
-    try:
-        print(mailList[i][0], mailList[i][1], mailList[i][2])
-    except:
-        pass
-
-ID = ''
-PW = ''
-
-server = imaplib.IMAP4_SSL('imap.daum.net')
-server.login(ID, PW)
-
-rv, data = server.select()
-recent_no = data[0]
-
-rv, fetched = server.fetch(recent_no, '(RFC822)')
-message = email.message_from_bytes(fetched[0][1])
-
-fr = make_header(decode_header(message.get('From')))
-print(fr)
-
-subject = make_header(decode_header(message.get('Subject')))
-print(subject)
-'''
