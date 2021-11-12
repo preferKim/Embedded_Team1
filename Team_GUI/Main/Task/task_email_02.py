3908cdeb20450023e2f1905a68df8ef35e6a67ef
Balparang <shineecard5@gmail.com>
2021-11-12 17:02:41 +0900

    이메일 연동 완료. rasp구동 확인 완료

--- Team_GUI/Main/Task/task_email_02.py
+++ Team_GUI/Main/Task/task_email_02.py
@@ -17,8 +17,8 @@ class emailWindow(QDialog):
         self.setupUI()
         
         # 사용자 이메일(@daum.net제외), 비밀번호 입력
-        self.user_id = "nalbojima"
-        self.user_pw = "2agl2sn2st#"
+        self.user_id = "다음 아이디"
+        self.user_pw = "다음 패스워드"
         self.current_row = 0
         self.num_to_load = 24 # 초기화시에 처음에 이메일 몇개 불러올건지에 대한 갯수
         
