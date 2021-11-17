e8fa49070d1ca9859a1a1e76a39749b700d9f9b0
floorbear2723 <floorbear2723@gmail.com>
2021-11-17 19:38:38 +0900

    Calender Update

--- Team_SW/Calender/Calender.py
+++ Team_SW/Calender/Calender.py
@@ -1,10 +1,11 @@
-# -*- coding: utf-8 -*-
+# -*- coding: euc-kr -*-
 from google_auth_oauthlib.flow import InstalledAppFlow #앱 인증을 위한 라이브러리
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.helpers import credentials_from_session
 from googleapiclient.discovery import build
 import pickle
 import os.path
+
 
 
 #----------------------------------------------- 사용자 인증------------------------------------------------------------
@@ -36,16 +37,41 @@ if not creds or not creds.valid:
 
 #----------------------------------------------- 서비스 사용------------------------------------------------------------
 import datetime
-
-today = datetime.date.today().isoformat() #현재 날짜 호출
-time_min = today + 'T00:00:00+09:00'#UTC+9
-time_max = today + 'T23:59:59+09:00'
-service = build('calendar', 'v3', credentials=creds) #Api에 대한 서비스 객체 생성
-print('Show today s schedule.')
-events_result = service.events().list(calendarId='primary',singleEvents=True,timeMin=time_min,timeMax=time_max,maxResults=10,orderBy='startTime').execute()
-
-events = events_result.get('items')
-print(events[0])
+from dateutil import relativedelta
+import dateutil.parser
 
 
 
+
+def GetEvents_1(): #오늘 날짜부터 이번달 말까지의 이벤트를 배열형태로 리턴해주는 함수
+    Event_List = []
+    today = datetime.date.today()
+    #현재 시간
+    now = datetime.datetime(today.year, today.month, today.day, 0, 0, 0).isoformat() + 'z'
+    #이번달의 첫번째 날
+    first_month = datetime.datetime(today.year,today.month,1,)
+    #다음달
+    next_month = first_month + relativedelta.relativedelta(months=1)
+    #이번달의 마지막날
+    last_day = (next_month - datetime.timedelta(seconds=1)).isoformat() + 'z'
+
+
+    service = build('calendar', 'v3', credentials=creds) #Api에 대한 서비스 객체 생성
+    print('Show today s schedule.')
+    events_result = service.events().list(calendarId='primary',singleEvents=True,timeMin=now,timeMax=last_day,maxResults=10,orderBy='startTime').execute()
+    events = events_result.get('items')
+
+    if not events:
+        print("Warning From Calender : 가져올 수 있는 이벤트가 없습니다")
+
+
+    for event in events:
+        start = event['start'].get('dateTime',event['start'].get('date'))
+        parsedDate = dateutil.parser.parse(start) #이벤트의 문자열화된 시간정보를 다시 dateTime으로 바꿔준다
+        t = (event['summary'],parsedDate)
+        Event_List.append(t)
+        
+    return Event_List
+
+
+print(GetEvents_1())
